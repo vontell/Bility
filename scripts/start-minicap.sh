2efb743eec8d90450aa24d3bf18e18ab30eb72c8
vontell <vontell@mit.edu>
2020-11-14 22:48:14 -0500

    Slightly better builds for minicap

--- scripts/start-minicap.sh
+++ scripts/start-minicap.sh
@@ -1,4 +1,8 @@
+source ~/.bility
+
 echo Starting up minicap...
+
+kill -9 $(lsof -t -i:9002) || true;
 
 cd ../dependencies/minicap/
 ./run.sh -P 1080x1920@810x1440/0 >> ../../log.txt &
@@ -14,5 +18,7 @@ until $(curl --output /dev/null --silent --head --fail localhost:9002); do
     printf '.'
     sleep 1
 done
+
+trap 'kill -9 $(lsof -t -i:9002) || true; echo Killed Bility frontend server' 0
 
 echo Minicap casting server started
