04d92a6389c7f254af1200b129e2e2623c4c510f
vontell <vontell@mit.edu>
2020-12-16 23:56:24 -0500

    FatJar for bility server

---
+++ server-start.sh
@@ -0,0 +1,1 @@
+java -server -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:InitialRAMFraction=2 -XX:MinRAMFraction=2 -XX:MaxRAMFraction=2 -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+UseStringDeduplication -jar bility-server/jars/bility-server.jar
